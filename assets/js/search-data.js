aae369c33ef52e66bc485a32f1d2495eaa56775e
yiruyang2025 <162698398+yiruyang2025@users.noreply.github.com>
2025-05-08 18:46:05 +0000

    Deploying to gh-pages from @ yiruyang2025/yiruyang2025.github.io@fd263eed036e17a6871d7b31ad991a19bc95ef84 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -41,7 +41,7 @@ ninja.data = [{
         
           title: "Speech Processing - 25",
         
-        description: "⛺️",
+        description: "🥥",
         section: "Posts",
         handler: () => {
           
