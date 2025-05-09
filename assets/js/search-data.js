3b66360aab13f5e8b8089521c540278d30337386
yiruyang2025 <162698398+yiruyang2025@users.noreply.github.com>
2025-05-09 09:16:07 +0000

    Deploying to gh-pages from @ yiruyang2025/yiruyang2025.github.io@a0d4f925a4f0775466d7791cb1f395f6f7a32f5d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -74,7 +74,7 @@ ninja.data = [{
         
           title: "Pancake Time with Chess Puzzles",
         
-        description: "🥞",
+        description: "🥥",
         section: "Posts",
         handler: () => {
           
@@ -107,7 +107,7 @@ ninja.data = [{
         
           title: "Lessons Learnt from parents and grandparents",
         
-        description: "🦩",
+        description: "🩵",
         section: "Posts",
         handler: () => {
           
