47a5c6242c06a82485498210f398f82ffacdb0ab
yiruyang2025 <162698398+yiruyang2025@users.noreply.github.com>
2025-05-15 11:51:18 +0000

    Deploying to gh-pages from @ yiruyang2025/yiruyang2025.github.io@83659b8ffccac6065a86b546fe9e533c94fa6a50 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -157,6 +157,11 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
+            },},{id: "projects-project-2",
+          title: 'project 2',
+          description: "a project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/2_project/";
             },},{
         id: 'social-email',
         title: 'email',
